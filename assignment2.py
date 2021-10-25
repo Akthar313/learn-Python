8aef946d6021052f9b53c3f5c4b0ba89565be901
h4ck€r313 <48244891+Akthar313@users.noreply.github.com>
2021-10-25 20:14:24 +0530

    Create assignment2.py
    #This program has a lot of errors, and needs to type cast some variable. Can you fix this?
    # Also fill the empty # with your own comment
    #There are total 7 errors

---
+++ assignment2.py
@@ -0,0 +1,18 @@
+#This program has a lot of errors, and needs to type cast some variable. Can you fix this?
+# Also fill the empty # with your own comment
+#There are total 7 errors
+
+# Gathers name of the user
+name = input("Enter your name: "
+#
+num1 = input("Enter an Integer: )
+#
+num2 = inpu("Enter an Floating value: ")
+#
+    sum = num1 + num2
+#
+print(type(sum)
+#
+print("Sum of num1 + num2 ="sum)
+#
+prit(len(name))
